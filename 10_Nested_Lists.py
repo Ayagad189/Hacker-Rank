a5edd3455f85944ee26fa669a8d14dd4cd07ee7d
Ayagad189 <126031812+Ayagad189@users.noreply.github.com>
2025-12-19 20:49:54 +0200

    Add Nested Lists solution

---
+++ 10_Nested_Lists.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    records = []
+
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        records.append([name, score])
+
+    scores = sorted(set(score for name, score in records))
+    second_lowest = scores[1]
+
+    names = [name for name, score in records if score == second_lowest]
+    names.sort()
+
+    for name in names:
+        print(name)
